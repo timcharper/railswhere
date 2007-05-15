c752092dfa8b2117cd4b33151cc0cb2fb4ed8197
timcharper <timcharper@18a020dd-8730-0410-9ac0-8dc635d5cadf>
2007-05-15 21:46:13 +0000

    rdoc documentation
    git-svn-id: http://railswhere.googlecode.com/svn/trunk@5 18a020dd-8730-0410-9ac0-8dc635d5cadf

--- lib/where.rb
+++ lib/where.rb
@@ -29,17 +29,31 @@
 # ===  Inline
 #  
 #    User.find(:all, :conditions => Where.new('first_name like ?', 'Tim').and('last_name like ?', 'Harper') )
+#    # Sweet chaining action!
 
 
 class Where
   
-  
+  # Constructs a new where clause
+  # 
+  # optionally, you can provide a criteria, like the following:
+  #   
+  #   Where.initialize "joke_title = ?", "He says, 'Under there', to which I reply, 'under where?'"
   def initialize(criteria=nil, *params)
     @clauses=Array.new
     
     self.and(criteria, *params) unless criteria.nil?
   end
   
+  # Appends an <b>and</b> expression to your where clause
+  # 
+  # Example:
+  #   
+  #   where = Where.new
+  #   where.and("name = ?", "Tim O'brien")
+  #   where.to_s
+  #   
+  #   # => "(name = 'Tim O''brien')
   def and(criteria, *params)
     criteria = [criteria] + params unless params.empty?
     @clauses << Clause.new(criteria) unless criteria.blank?
@@ -48,12 +62,23 @@ class Where
   
   alias << and
   
+  # Appends an <b>or</b> expression to your where clause
+  # 
+  # Example:
+  #   
+  #   where = Where.new
+  #   where.or("name = ?", "Tim O'brien")
+  #   where.or("name = ?", "Tim O'neal")
+  #   where.to_s
+  #   
+  #   # => "(name = 'Tim O''brien') or (name = 'Tim O''neal')"
   def or(criteria, *params)
     criteria = [criteria] + params unless params.empty?
     @clauses << Clause.new(criteria, true) unless criteria.blank?
     self
   end
   
+  # Converts the where clause to a SQL string.
   def to_s(format=nil)
     output=""
     
@@ -69,12 +94,32 @@ class Where
     end
   end
   
-  def empty?
+  alias :to_sql :to_s
+  
+  # Determines if any clauses have been added.
+  # 
+  #   where = Where.new
+  #   where.blank?
+  #   # => true
+  #   
+  #   where.and(nil)
+  #   where.blank?
+  #   # => true
+  #   
+  #   where.and(Where.new(nil))
+  #   where.blank?
+  #   # => true
+  #   
+  #   where.and("name=1")
+  #   where.blank?
+  #   # => false
+  def blank?
     @clauses.empty?
   end
   
-  alias :blank? :empty? 
+  alias :empty? :blank? 
    
+  # Used internally to +Where+.   You shouldn't have any reason to interact with this class. 
   class Clause
     def initialize(criteria, is_or = false)
       @is_or=is_or
